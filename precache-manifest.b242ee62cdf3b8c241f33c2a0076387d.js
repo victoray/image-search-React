7706c72372323ac974bbfcbcf2c5e382f4d7cefc
Victor Ay <viktoray007@gmail.com>
2019-10-13 06:07:37 +0300

    Updates

---
+++ precache-manifest.b242ee62cdf3b8c241f33c2a0076387d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42886034a834687bbd4ba509ddd759ef",
+    "url": "/image-search-React/index.html"
+  },
+  {
+    "revision": "039262f4b7dfa7d24ec9",
+    "url": "/image-search-React/static/css/main.cf54666a.chunk.css"
+  },
+  {
+    "revision": "c7a5d7ed604e0c8bbdd0",
+    "url": "/image-search-React/static/js/2.b1fbe159.chunk.js"
+  },
+  {
+    "revision": "039262f4b7dfa7d24ec9",
+    "url": "/image-search-React/static/js/main.ea467b1b.chunk.js"
+  },
+  {
+    "revision": "5870fcca0a14f9c85f5e",
+    "url": "/image-search-React/static/js/runtime-main.523cd8ef.js"
+  }
+]);
